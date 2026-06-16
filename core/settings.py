b295aa5115c62ce4a3903373db1cbd7eedb6a072
arthp <agrearth22@gmail.com>
2026-06-16 21:47:29 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMIddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
