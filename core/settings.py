d3275cbf448652db2903903ebe46c080529ae5f4
arthp <agrearth22@gmail.com>
2026-06-17 11:13:52 +0530

    design added with colour

--- core/settings.py
+++ core/settings.py
@@ -69,17 +69,17 @@ JAZZMIN_SETTINGS = {
     },
 }
 
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 CORS_ALLOWED_ORIGINS = [
     "https://hkmarketinghouse.vercel.app",
@@ -155,3 +155,4 @@ DJANGO_SUPERUSER_PASSWORD = "hkmarketing736"
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
